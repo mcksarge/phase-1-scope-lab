a4efd59c93ca88223a5fdde7130cf104e9c44fa1
Fred <higgins.fred.s@gmail.com>
2021-11-29 21:03:00 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer;
+const leastFavoriteCustomer = '';
+
+//Makes customer name upper case
+function upperCaseCustomerName () {
+    customerName = customerName.toUpperCase();
+}
+
+//Creates a global variable when called
+function setBestCustomer () {
+    bestCustomer = 'not bob';
+}
+
+//Changes best customer
+function overwriteBestCustomer () {
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = 'bobby boy'
+}
